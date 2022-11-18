b2cbcbd8590dd501adb6f12f111bc4c6f045ac53
joyfuljune <88413945+joyfuljune@users.noreply.github.com>
2022-11-18 15:58:30 +0800

    Add files via upload

---
+++ 作业2/ex2.m
@@ -0,0 +1,64 @@
+%% Exercise 2 MATLAB FOR BEGINNERS
+%%
+function [stu_id, stu_name, Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10,Q11,Q12,Q13,q1,q2,q3,q4,q5] = ex2
+    stu_id = 1234567890;    % change to your student id
+    stu_name = 'fake_name'; % replace with your name inside ''
+    Q1 = @tri_area;      % please do not edit a single character from thie line below
+    Q2 = @corners;     % necessary for grading
+    Q3 = @taxi_fare;
+    Q4 = @minimax;
+    Q5 = @trio;
+    Q6 = @picker;
+    Q7 = @eligible;
+    Q8 = @under_age;
+    Q9 = @valid_date;
+    Q10 = @halfsum;
+    Q11 = @next_prime;
+    Q12 = @freezing;
+    Q13 = @max_sum;
+    q1 = @circle;
+    q2 = @flip_it;
+    q3 = @zero_stat;
+    q4 = @sum3and5;
+    q5 = @move_me;
+end
+
+%% part 1 exercise
+%Q1
+function area = tri_area(b,h)
+    area = b*h/2;
+end
+
+%Q2
+
+%Q3
+
+%Q4
+
+%Q5
+
+%Q6
+
+%Q7
+
+%Q8
+
+%Q9
+
+%Q10
+
+%Q11
+
+%Q12
+
+%Q13
+
+%q1
+
+%q2
+
+%q3
+
+%q4
+
+%q5
