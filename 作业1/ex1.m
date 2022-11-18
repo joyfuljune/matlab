73a970662f75aa8595481b05a780cc64cbacb3a6
joyfuljune <88413945+joyfuljune@users.noreply.github.com>
2022-11-18 15:52:02 +0800

    Add files via upload

---
+++ 作业1/ex1.m
@@ -0,0 +1,35 @@
+%% Exercise 1 MATLAB FOR BEGINNERS
+%%
+stu_id = 1234567890;
+stu_name = 'zhangsan';
+
+%% part 1
+a = 10;
+
+%% part 2
+
+
+%% part 3
+
+
+%% part 4
+
+
+%% Image
+
+% Q5
+figure(1);
+
+% Q6
+figure(2);
+
+% Q7
+figure(3);
+
+
+figure(4);
+
+
+
+%% exercise without grading
+
